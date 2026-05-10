765b706b0868ee6364d632214eb464269c0365b5
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-05-10 15:52:15 +0300

    Driver: Create DriverData and DriverService

---
+++ Ice Cream Distribution System/Business/DriverService.cs
@@ -0,0 +1,18 @@
+﻿using Data_Access;
+using Ice_Cream_Distribution_System.Models;
+
+namespace Business
+{
+    public static class DriverService
+    {
+        public static async Task<Driver?> Add(Driver driver) => await DriverData.Add(driver);
+
+        public static async Task<bool> Update(Driver driver) => await DriverData.Update(driver);
+
+        public static async Task<bool> Delete(int id) => await DriverData.Delete(id);
+
+        public static async Task<Driver?> Get(int id) => await DriverData.Get(id);
+
+        public static async Task<List<Driver>?> GetAll() => await DriverData.GetAll();
+    }
+}
---
+++ Ice Cream Distribution System/Data Access/DriverData.cs
@@ -0,0 +1,116 @@
+﻿using Golbal.DBContext;
+using Ice_Cream_Distribution_System.Models;
+using Microsoft.EntityFrameworkCore;
+using MoneyMindManagerGlobal;
+
+namespace Data_Access
+{
+    public static class DriverData
+    {
+        public static async Task<Driver?> Add(Driver driver)
+        {
+            if (driver is null)
+                return null;
+
+            try
+            {
+                using (var context = new IceCreamDistributionDbContext())
+                {
+                    await context.Drivers.AddAsync(driver);
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                clsGlobalEvents.RaiseErrorEvent(ex.Message);
+                return null;
+            }
+
+            return driver;
+        }
+
+        public static async Task<bool> Update(Driver driver)
+        {
+            if (driver is null)
+                return false;
+
+            try
+            {
+                using (var context = new IceCreamDistributionDbContext())
+                {
+                    context.Drivers.Update(driver);
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                clsGlobalEvents.RaiseErrorEvent(ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public static async Task<bool> Delete(int driverID)
+        {
+            try
+            {
+                using (var context = new IceCreamDistributionDbContext())
+                {
+                    var driver = await context.Drivers.FindAsync(driverID);
+                    if (driver == null) return false;
+
+                    context.Drivers.Remove(driver);
+                    await context.SaveChangesAsync();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                clsGlobalEvents.RaiseErrorEvent("لا يمكن حذف السائق لارتباطه بورديات سابقة: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static async Task<Driver?> Get(int driverID)
+        {
+            try
+            {
+                using (var context = new IceCreamDistributionDbContext())
+                {
+                    return await context.Drivers
+                        .Include(d => d.User)
+                            .ThenInclude(u => u.Person)
+                        .Include(d => d.Car)
+                        .FirstOrDefaultAsync(d => d.Id == driverID);
+                }
+            }
+            catch (Exception ex)
+            {
+                clsGlobalEvents.RaiseErrorEvent(ex.Message);
+                return null;
+            }
+        }
+
+        public static async Task<List<Driver>?> GetAll()
+        {
+            try
+            {
+                using (var context = new IceCreamDistributionDbContext())
+                {
+                    return await context.Drivers
+                        .Include(d => d.User)
+                            .ThenInclude(u => u.Person)
+                        .Include(d => d.Car)
+                        .AsNoTracking()
+                        .ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                clsGlobalEvents.RaiseErrorEvent(ex.Message);
+                return null;
+            }
+        }
+    }
+}
