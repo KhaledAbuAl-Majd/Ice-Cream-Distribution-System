3fe0bf23dd6d0ca1b7ba66938fbe4563504a5453
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-05-10 15:49:16 +0300

    Car: Create CarData and CarService

---
+++ Ice Cream Distribution System/Business/CarServcie.cs
@@ -0,0 +1,18 @@
+﻿using Data_Access;
+using Ice_Cream_Distribution_System.Models;
+
+namespace Business
+{
+    public static class CarService
+    {
+        public static async Task<Car?> Add(Car car) => await CarData.Add(car);
+
+        public static async Task<bool> Update(Car car) => await CarData.Update(car);
+
+        public static async Task<bool> Delete(short id) => await CarData.Delete(id);
+
+        public static async Task<Car?> Get(short id) => await CarData.Get(id);
+
+        public static async Task<List<Car>?> GetAll() => await CarData.GetAll();
+    }
+}
---
+++ Ice Cream Distribution System/Data Access/CarData.cs
@@ -0,0 +1,112 @@
+﻿using Golbal.DBContext;
+using Ice_Cream_Distribution_System.Models;
+using Microsoft.EntityFrameworkCore;
+using MoneyMindManagerGlobal;
+
+namespace Data_Access
+{
+    public static class CarData
+    {
+        public static async Task<Car?> Add(Car car)
+        {
+            if (car is null)
+                return null;
+
+            try
+            {
+                using (var context = new IceCreamDistributionDbContext())
+                {
+                    await context.Cars.AddAsync(car);
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                clsGlobalEvents.RaiseErrorEvent(ex.Message);
+                return null;
+            }
+
+            return car;
+        }
+
+        public static async Task<bool> Update(Car car)
+        {
+            if (car is null)
+                return false;
+
+            try
+            {
+                using (var context = new IceCreamDistributionDbContext())
+                {
+                    context.Cars.Update(car);
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                clsGlobalEvents.RaiseErrorEvent(ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public static async Task<bool> Delete(short carID)
+        {
+            try
+            {
+                using (var context = new IceCreamDistributionDbContext())
+                {
+                    var car = await context.Cars.FindAsync(carID);
+                    if (car == null) return false;
+
+                    context.Cars.Remove(car);
+                    await context.SaveChangesAsync();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                clsGlobalEvents.RaiseErrorEvent("لا يمكن حذف السيارة لارتباطها ببيانات أخرى: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static async Task<Car?> Get(short carID)
+        {
+            try
+            {
+                using (var context = new IceCreamDistributionDbContext())
+                {
+                    return await context.Cars
+                        .Include(c => c.Area)
+                        .FirstOrDefaultAsync(c => c.Id == carID);
+                }
+            }
+            catch (Exception ex)
+            {
+                clsGlobalEvents.RaiseErrorEvent(ex.Message);
+                return null;
+            }
+        }
+
+        public static async Task<List<Car>?> GetAll()
+        {
+            try
+            {
+                using (var context = new IceCreamDistributionDbContext())
+                {
+                    return await context.Cars
+                        .Include(c => c.Area)
+                        .AsNoTracking()
+                        .ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                clsGlobalEvents.RaiseErrorEvent(ex.Message);
+                return null;
+            }
+        }
+    }
+}
